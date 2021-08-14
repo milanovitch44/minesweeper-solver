f4ffb60b92602971b06e00ab8715911bdef317cc
milanovitch44 <milan.pickavet@gmail.com>
2021-08-14 17:20:04 +0200

    fix DFS & fix getBoard() height-width

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from random import randrange
-
 class MineField:
     def __init__(self,width,height,bombs) -> None:
         self.board=[[-1]*height for _ in range(width)]
         self.isBomb = [[False]*height for _ in range(width)]
         self.width,self.height = width,height
+        self.tilesToOpen = []
+
         while bombs!=0:
             x,y = randrange(0,width),randrange(0,height)
             if not self.isBomb[x][y]:
@@ -24,20 +25,27 @@ class MineField:
         return out
 
     def getBoard(self)->str:
-        return "\t\t"+"\n\t\t".join(["".join([self.getTileChar(tile) for tile in row]) for row in self.board])
+        return "\t\t"+"\n\t\t".join(["".join(
+                [self.getTileChar(self.board[x][y]) for x in range(self.width)]
+            ) for y in range(self.height)])
     def getTileChar(self,number):
         return "." if number==0 else "#" if number==-1 else str(number)
     def openTile(self,x,y)->bool:
         """
     @return True if succesfull
     """
+        assert len(self.tilesToOpen)==0
+        self.tilesToOpen=[(x,y)]
         if self.isBomb[x][y]:
-            return False
-        self.board[x][y]=self.countBombs(x,y)
-        if self.board[x][y]==0:
-            for x,y in self.getNeighbours(x,y):
-                self.openTile(x,y)
-
+                return False
+        while len(self.tilesToOpen)!=0:
+            x,y = self.tilesToOpen.pop()
+            self.board[x][y]=self.countBombs(x,y)
+            if self.board[x][y]==0:
+                for x,y in self.getNeighbours(x,y):
+                    if self.board[x][y]==-1:#not visited yet
+                        self.tilesToOpen.append((x,y))
+            
         return True
     
     def countBombs(self,tileX,tileY):
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import main
 
 # print(main.MineField(2,3,4).board)
-mf = main.MineField(4,4,bombs=1)
-print(mf.openTile(0,0))
-print(mf.board)
+mf = main.MineField(40,10,bombs=50)
+mf.openTile(0,0)
+# print(mf.board)
 print(mf.getBoard())
