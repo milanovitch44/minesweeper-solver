84ed8c3de48aa2e98c78753ae9ff6b90a4ca3015
milanovitch44 <milan.pickavet@gmail.com>
2021-08-30 17:49:39 +0200

    flood fill works - engine still in development

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from random import randrange
+import heapq
+import time
 class MineField:
     def __init__(self,width,height,bombs) -> None:
         self.board=[[-1]*height for _ in range(width)]
@@ -11,8 +13,10 @@ class MineField:
             if not self.isBomb[x][y]:
                 bombs-=1
                 self.isBomb[x][y]=True
-        
-    def getNeighbours(self,tileX,tileY):
+    def getCoordinate(self,coordinate:list):
+        return self.board[coordinate[0]][coordinate[1]]
+    def getNeighbours(self,coordinate:list):
+        tileX,tileY=coordinate
         out=[]
         for xOffset,yOffset in (
             (-1,-1),(1,-1),(-1,1),(1,1),
@@ -28,30 +32,122 @@ class MineField:
         return "\t\t"+"\n\t\t".join(["".join(
                 [self.getTileChar(self.board[x][y]) for x in range(self.width)]
             ) for y in range(self.height)])
-    def getTileChar(self,number):
-        return "." if number==0 else "#" if number==-1 else str(number)
-    def openTile(self,x,y)->bool:
+
+    getTileChar = lambda self,number:"." if number==0 else "#" if number==-1 else str(number)
+    def openTile(self,beginCoordinate)->bool:
         """
     @return True if succesfull
     """
+        beginX,beginY = beginCoordinate
         assert len(self.tilesToOpen)==0
-        self.tilesToOpen=[(x,y)]
-        if self.isBomb[x][y]:
-                return False
+        self.tilesToOpen=[beginCoordinate]
+        if self.isBomb[beginX][beginY]:
+            return False
         while len(self.tilesToOpen)!=0:
-            x,y = self.tilesToOpen.pop()
-            self.board[x][y]=self.countBombs(x,y)
+            
+            coordinate = self.tilesToOpen.pop()
+            x,y = coordinate
+            if self.board[x][y]!=self.countBombs(coordinate):
+                print(self.getBoard(),end="\n\n\n\n\n\n",flush=True)
+                # time.sleep(0.00001)
+            self.board[x][y]=self.countBombs(coordinate)
+            
             if self.board[x][y]==0:
-                for x,y in self.getNeighbours(x,y):
-                    if self.board[x][y]==-1:#not visited yet
-                        self.tilesToOpen.append((x,y))
+                for nextX,nextY in self.getNeighbours(coordinate):
+                    if self.board[nextX][nextY]==-1:#not visited yet
+                        self.tilesToOpen.append((nextX,nextY))
             
         return True
     
-    def countBombs(self,tileX,tileY):
+    def countBombs(self,coordinate:list):
+        tileX,tileY = coordinate
         counter = 0
-        for x,y in self.getNeighbours(tileX,tileY):
+        for x,y in self.getNeighbours((tileX,tileY)):
             if self.isBomb[x][y]:
                 counter+=1
         return counter
+
+class fieldChange:
+    def __init__(self,isBomb,coordinates) -> None:
+        self.isBomb=isBomb
+        self.coordinates=coordinates
+    def __str__(self) -> str:
+        return f"fieldChange({self.getType()} on {self.coordinates})"
+    getType = lambda self:"flagged bomb" if self.isBomb else "safe spot"
+
+class Engine:
+    def __init__(self,field:MineField) -> None:
+        self.calculateEdgeTiles(field)
+        self.changesToEvaluate:heapq.__heap = [[]]
+
+
+    def calculateEdgeTiles(self,field:MineField):
+        self.unknownEdgeTiles = set()
+        self.knownEdgeTiles = {}
+        for x,row in enumerate(field.board):
+            for y,el in enumerate(row):
+                if field.board[x][y]!=-1:
+                    for neighbour in field.getNeighbours((x,y)):
+                        if field.getCoordinate(neighbour)==-1:
+                            self.unknownEdgeTiles.add(neighbour)
+                    self.knownEdgeTiles[(x,y)] = fieldChange(False,(x,y))
+                       
+    def isValid(self,field:MineField,changes:list[fieldChange]):
+        neighbours = self.getNeighboursOfFieldChange(field,changes)
+        for changes,coordinate in neighbours.items():
+            upperLimit = 8
+            lowerLimit = 0
+            for neighbour in field.getNeighbours(coordinate):
+                if neighbour in self.knownEdgeTiles:
+                    if self.knownEdgeTiles[neighbour].isBomb:
+                        lowerLimit+=1
+                    else:
+                        upperLimit-=1
+            for change in changes:
+                if change.isBomb:
+                    lowerLimit+=1
+                else:
+                    upperLimit-=1
+            surroundingBombs = field.getCoordinate(coordinate)
+            if lowerLimit>surroundingBombs or upperLimit<surroundingBombs:
+                return False
+        return True
+
+    def getNeighboursOfFieldChange(self,field:MineField,changes:list[fieldChange])->dict[list[fieldChange]]:
+        neighbours = {}
+        for change in changes:
+            for neighbour in field.getNeighbours(change.coordinates):
+                if field.getCoordinate(neighbour)!=-1:
+                    if neighbour in neighbours:
+                        neighbours[neighbour].append(change)
+                    else:
+                        neighbours[neighbour]=[change]
+        return neighbours
+    def getPossibleChanges(self,field:MineField,changes:list[fieldChange])->set[list]:
+        neighbours=set()
+        for change in changes:
+            for localNeighbour in field.getNeighbours(change.coordinates):
+                if field.getCoordinate(localNeighbour)!=-1:
+                    for farNeighbour in field.getNeighbours(localNeighbour):
+                        if field.getCoordinate(farNeighbour)==-1:
+                            neighbours.add(farNeighbour)
+
+                        # if field.board[neighbour[0]][neighbour[1]]!=-1:
+                        #     if neighbour in neighbours:
+                        #         neighbours[neighbour].append(change)
+                        #     else:
+                        #         neighbours[neighbour]=[change]
+        # changes = [fieldChange(False,coordinates) for coordinates in ]
+        return neighbours
+    def getNextTile(self,field:MineField)->fieldChange:
+        while len(self.changesToEvaluate)!=0:
+            changeList = heapq.heappop(self.changesToEvaluate)
+            neighbours = self.getPossibleChanges(field,changeList)
+            print(neighbours)
         
+
+        return None
+
+
+
+    
--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 import main
 
 # print(main.MineField(2,3,4).board)
-mf = main.MineField(40,10,bombs=50)
-mf.openTile(0,0)
-# print(mf.board)
+mf = main.MineField(120,37,bombs=200)
+mf.openTile((3,3))
 print(mf.getBoard())
+e=main.Engine(mf)
+print(e.getNextTile(mf))
+# print(mf.board)
+
+# print(list(main.Engine().calculateEdgeTiles(mf)))
+# e = main.Engine()
+# print(r:=e.isValid(mf,[main.fieldChange(True,(1,0)),main.fieldChange(False,(0,0))]))
+# print(", ".join([str(el) for el in r[(0,1)]]))
